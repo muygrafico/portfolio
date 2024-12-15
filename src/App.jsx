74df523d3e3cd56d909a348863edaacdba46c52a
German Hernandez <gh@192.168.1.21>
2024-12-14 21:04:31 -0500

    update portfolio info

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import railsLogo from '/rails-logo-circle.png'
+import germanThumbnail from '/german-thumbnail-bw.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className="logos">
+        <a href="https://www.linkedin.com/in/muygrafico" target="_blank">
+          <img src={germanThumbnail} className="logo thumbnail" alt="Developer thumbnail" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://rubyonrails.org" target="_blank">
+          <img src={railsLogo} className="logo rails" alt="Rails logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>German + React</h1>
+      <h3>Innovative Fullstack Developer | Bridging Creativity and Technology</h3>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <p className="paragraph-1">
+          My name is <strong>German</strong>, a fullstack developer with a passion for building intuitive and visually compelling web applications. With over a decade of experience in design and development, I bring a unique blend of creativity and technical expertise to your project.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the logos to learn more
       </p>
     </>
   )
